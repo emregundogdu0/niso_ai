105d47eb624145d2e23e6d93025936bb8edd4c6e
Emre Gundogdu <emre.gundogdu016@gmail.com>
2026-09-02 13:11:00 +0300

    feat(ui): add ELDOR-logo.svg alongside NISO logo in hero header

--- ui/server.js
+++ ui/server.js
@@ -208,7 +208,12 @@ const server = http.createServer(async (req, res) => {
     const mimeTypes = {
       '.html': 'text/html; charset=utf-8',
       '.css': 'text/css; charset=utf-8',
-      '.js': 'application/javascript; charset=utf-8'
+      '.js': 'application/javascript; charset=utf-8',
+      '.svg': 'image/svg+xml',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.ico': 'image/x-icon'
     };
     res.writeHead(200, { 'Content-Type': mimeTypes[ext] || 'text/plain' });
     fs.createReadStream(filePath).pipe(res);
